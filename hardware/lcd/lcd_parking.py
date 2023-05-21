7dae7b9fa0b772c860e516de04e0e6daaf865a1a
alouradou <pi@rasberry.com>
2023-05-21 17:43:03 +0200

    Code LCD

---
+++ hardware/lcd/lcd_parking.py
@@ -0,0 +1,8 @@
+import I2C_LCD_driver
+from time import *
+
+
+class LcdParking:
+    def __init__(self,message):
+        self.mylcd = I2C_LCD_driver.lcd()
+        self.mylcd.lcd_display_string(str(message), 1)
