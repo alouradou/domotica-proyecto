30ae2f7d83f8e678863cdc30442c4ff37ae879ce
alouradou <pi@rasberry.com>
2023-05-25 03:41:15 +0200

    Class MQTTClient

--- mqtt/sub.py
+++ mqtt/sub.py
@@ -9,19 +9,19 @@ class MQTTClient:
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.connect(broker_address, port, 60)
-        self.client.loop_start()
+        self.client.loop_forever()
 
     # The callback for when the client receives a CONNACK response from the server.
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
 
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
         # client.subscribe("$SYS/#")
 
-        client.subscribe("upm/domotica/test/#")
+        self.client.subscribe("upm/mqtt/#")
 
     # The callback for when a PUBLISH message is received from the server.
-    def on_message(client, userdata, msg):
+    def on_message(self, client, userdata, msg):
         print(msg.topic + " " + str(msg.payload.decode("utf-8")))
 
