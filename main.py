0fc8e9a5699edc2b6c5704a4c70b4e5cc79dbb07
Arthur L <18040415+alouradou@users.noreply.github.com>
2023-05-25 03:29:19 +0200

    Connection main mqtt

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
-from hardware.rfid.rfid_reader import RFIDReader
+# from hardware.rfid.rfid_reader import RFIDReader
+import threading
+import time
+import paho.mqtt.client as mqtt
+
+def rfid_loop():
+    while True:
+        print("RFID loop: No card")
+        time.sleep(5)
+
+def mqtt_listen():
+    print("MQTT listen")
 
 
 def main():
-    import threading
-    import time
-    import paho.mqtt.client as mqtt
 
-    # Importez votre classe RFIDReader ici
-    # from rfid_reader import RFIDReader
-
-    # Créez une instance de la classe RFIDReader
-    rfid_reader = RFIDReader()
-
-    # Fonction de rappel pour la réception des messages MQTT
-    def on_message(client, userdata, message):
-        # Traitez le message MQTT ici
-        print("Message reçu :", message.payload.decode())
-
-    # Fonction pour lancer l'écoute MQTT dans un thread
-    def mqtt_listen():
-        # Créez un client MQTT
-        client = mqtt.Client()
-
-        # Définissez les fonctions de rappel pour la connexion et la réception des messages
-        client.on_connect = lambda client, userdata, flags, rc: print("Connecté au broker MQTT")
-        client.on_message = on_message
-
-        # Connectez-vous au broker MQTT
-        client.connect("localhost", 1883, 60)
-
-        # Abonnez-vous à un ou plusieurs sujets MQTT
-        client.subscribe("topic1")
-        client.subscribe("topic2")
-
-        # Boucle principale MQTT (sera exécutée dans un thread séparé)
-        client.loop_forever()
-
-    # Fonction pour exécuter la boucle read_rfid dans un thread
-    def rfid_loop():
-        rfid_reader.read_rfid()
-
-    # Lancez les deux fonctions dans des threads séparés
     mqtt_thread = threading.Thread(target=mqtt_listen)
     rfid_thread = threading.Thread(target=rfid_loop)
 
     mqtt_thread.start()
     rfid_thread.start()
 
-    # Attendez indéfiniment que les threads se terminent (ne terminera jamais car ce sont des boucles infinies)
+    # Attendre indéfiniment que les threads se terminent
+    # (ne terminera jamais car ce sont des boucles infinies)
     while True:
         time.sleep(1)
 
--- mqtt/sub.py
+++ mqtt/sub.py
@@ -1,32 +1,27 @@
-#https://www.eclipse.org/paho/clients/python/docs/
-#Modified by: Paloma de la Puente
-
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+broker_address = "127.0.0.1"  # Use the Raspberry Pi IP address
+# broker_address = "test.mosquitto.org"
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
+class MQTTClient:
+    def __init__(self, broker_address=broker_address, port=1883):
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(broker_address, port, 60)
+        self.client.loop_start()
 
-    client.subscribe("upm/domotica/test/#")
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload.decode("utf-8")))
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        # client.subscribe("$SYS/#")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+        client.subscribe("upm/domotica/test/#")
 
-client.connect("test.mosquitto.org", 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
-
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(client, userdata, msg):
+        print(msg.topic + " " + str(msg.payload.decode("utf-8")))
 
